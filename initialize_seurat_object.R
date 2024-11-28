96280aa1007b9ec868165ce0239af9f2d3e65108
Søren Sandgaard <sorsa@dtu.dk>
2024-11-28 11:47:08 +0100

    removed arguments from R script

--- initialize_seurat_object.R
+++ initialize_seurat_object.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+# args = commandArgs(trailingOnly=TRUE)
 
 a <- data.frame(t1 = 1:5)
-a$t2 <- args[1]
-a$t3 <- args[2]
+# a$t2 <- args[1]
+# a$t3 <- args[2]
 
 saveRDS(a,file = "test.rds")
